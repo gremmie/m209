a50a18ce9623eb4540105747fc9a98cdeccf17d4
Brian Neal <bgneal@gmail.com>
2013-07-23 20:49:59 -0500

    Setting version to 0.1.0. Created MANIFEST.in.

--- m209/__init__.py
+++ m209/__init__.py
@@ -2,7 +2,7 @@
 # This file is part of m209, the M-209 simulation.
 # m209 is released under the MIT License (see LICENSE.txt).
 
-__version__ = '0.1'
+__version__ = '0.1.0'
 
 class M209Error(Exception):
     """Base Exception class for all M209 errors"""
